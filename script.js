3b7abf8679623eb12b627a1460bb512c82210f56
alessandro-gallinella <gallinellaalessandro850@gmail.com>
2025-12-18 11:34:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 console.log("inizio esecuzione")
 let d = new date();
 console.log(d)
+document.getElementById("outText").innerHTML = d.getDay();
 function elabora() {
 let numero = document.getElementById("inNumber").value;
 alert(numero);
-  
+}
 if (numero >=0 && numero <= 255){
   document.getElementById("outText").innerHTML = "hai inserito il numero corretto";
 color.push(numero);
@@ -13,7 +14,7 @@ document.getElementById("error").innerHTML = "hai inserito il"+numero;
 
 }
 else {
-  document.get ElementById("error").innerHTML = "il numero non è corretto";
+  document.getElementById("error").innerHTML = "il numero non è corretto";
 }
 
 function reset(){
