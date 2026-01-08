b2b82c582271f0564d4c7961c9615ab3deac5261
alessandro-gallinella <gallinellaalessandro850@gmail.com>
2026-01-08 11:13:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-console.log("inizio esecuzione")
-let d = new date();
-console.log(d)
-document.getElementById("outText").innerHTML = d.getDay();
+console.log("Inizio esecuzione");
+
+let d = new Date();
+console.log(d);
+
+document.getElementById("saluto").innerHTML =
+    d.getDate() + "/" + (d.getMonth()+1) + "/" + d.getFullYear();
+
+let colori = [];
+
 function elabora() {
-let numero = document.getElementById("inNumber").value;
-alert(numero);
+    let numero = Number(document.getElementById("inNumber").value);
+   
+    document.getElementById("error").innerHTML = "";
+   
+    if (numero >= 0 && numero <= 255) {
+        colori.push(numero);
+       
+        document.getElementById("outText").innerHTML =
+            "Hai inserito il numero corretto";
+        document.getElementById("outText").innerHTML +=
+            "<br>I colori inseriti sono: " + colori;
+       
+        console.log(colori);
+       
+        if (colori.length == 3) {
+            document.getElementById("inText").style.color =
+                `rgb(${colori[0]},${colori[1]},${colori[2]})`;
+        }
+    }
+    else {
+        document.getElementById("error").innerHTML = "Il numero non è corretto";
+    }
 }
-if (numero >=0 && numero <= 255){
-  document.getElementById("outText").innerHTML = "hai inserito il numero corretto";
-color.push(numero);
-  
-document.getElementById("error").innerHTML = "hai inserito il"+numero;
 
+function reset() {
+    colori = [];
+    document.getElementById("inNumber").value = "";
+    document.getElementById("inText").value = "";
+    document.getElementById("inText").style.color = "black";
+    document.getElementById("outText").innerHTML = "Inserisci il testo e tre valori RGB per colorarlo!";
+    document.getElementById("error").innerHTML = "";
 }
-else {
-  document.getElementById("error").innerHTML = "il numero non è corretto";
-}
-
-function reset(){
-   document.getElementById("inNumber").value = "";
-}  
-
